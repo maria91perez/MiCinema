a94b393a77609b48b0b9d31d5b3237b992db66d7
Guadalupe <213s0015@itsta.edu.mx>
2025-03-18 12:02:21 -0600

    Agregue agradecimiento al reservar voleto

--- resources/views/boleto/boleto.blade.php
+++ resources/views/boleto/boleto.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div class="container">
     <h1>Confirmación de Reserva para cada asistente</h1>
-    <p>Gracias por tu reserva. Aquí está tu boleto:</p>
+    <p>Gracias por tu reserva. Aquí le entrego su boleto gracias:</p>
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title">{{ $reserva->pelicula->titulo }}</h5>
+            <h5 class="card-title">{{ $reserva->Nombre de la pelicula->titulo }}</h5>
             <p><strong>Asientos:</strong> {{ implode(', ', json_decode($reserva->asientos)) }}</p>
             <p><strong>Proyección:</strong> {{ $reserva->proyeccion->hora_inicio }}</p>
             <img src="data:image/png;base64, {{ base64_encode(QrCode::format('png')->size(200)->generate(route('reservas.show', $reserva->id))) }}" alt="Código QR">
