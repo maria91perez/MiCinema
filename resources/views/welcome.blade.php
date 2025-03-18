04ab64a488bd946859cdb6e5453fd44a6641e0f7
Lorena <36lorenaperez@gmail.com>
2025-03-18 11:44:45 -0600

    hice cambios en la página principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,14 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Cine </a>
+            <a class="navbar-brand" href="#">Cinema</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="{{ route('cartelera') }}">Cartelera</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Iniciar Sesión</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Iniciar sesión</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Registrarse</a></li>
                 </ul>
             </div>
